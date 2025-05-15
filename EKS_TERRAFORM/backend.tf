3e879ecdb78e9ade8eb9efa4c90bf3618c6282fd
Charanlk <charanlk27@gmail.com>
2025-05-15 12:14:37 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-charan-1" # Replace with your actual S3 bucket name
+    bucket = "devsecops-tetris-charan-1"
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
